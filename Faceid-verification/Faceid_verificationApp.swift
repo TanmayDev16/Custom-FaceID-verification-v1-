5098f26bde082c388b974469daf379ef383c3247
TanmayDev16 <srivastava.tanmay.162003@gmail.com>
2023-01-09 01:36:11 +0530

    Initial Commit

---
+++ Faceid-verification/Faceid_verificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Faceid_verificationApp.swift
+//  Faceid-verification
+//
+//  Created by Tanmay on 09/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Faceid_verificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
